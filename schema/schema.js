5f4e837fc5d40731f526dbc0adb669ff3b3ae796
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-01-30 22:40:30 +0900

    Lecture 4, Async Resolve Functions

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,4 @@
-const _ = require('lodash');
+const axios = require('axios');
 const graphql = require('graphql');
 const {
   GraphQLObjectType,
@@ -28,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data);
       }
     }
   }
