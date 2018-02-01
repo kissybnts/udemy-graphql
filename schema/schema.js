f61c2c24694fe86f73bb90085622561307270171
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-01 23:33:09 +0900

    Lecture 21-13, Add users field to the company schema with resolving the circular references issue

--- schema/schema.js
+++ schema/schema.js
@@ -5,15 +5,22 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`).then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
